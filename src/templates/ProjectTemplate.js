21e9943c6a33499c861964d7778a14220d36a84c
ZehraRiz <zehrataqi@gmail.com>
2020-09-17 02:49:42 +0300

    nav aniamtion

--- src/templates/ProjectTemplate.js
+++ src/templates/ProjectTemplate.js
@@ -3,40 +3,14 @@ import { graphql, Link } from "gatsby";
 import Image from "gatsby-image";
 import { Layout } from "../components";
 
-const ProjectTemplate = ({
-	data: { project: { name, githubUrl, liveUrl, descriptionImages, description: { description } } }
-}) => {
+const ProjectTemplate = () => {
 	return (
 		<Layout>
-			<div>
-				<Link to="/projects">Back to projects</Link>
-				<h1>Single product: {name}</h1>
-				<article>
-					<Image fixed={descriptionImages[0].fixed} alt={name} />
-				</article>
-				<p>{description}</p>
-				<Image fixed={descriptionImages[1].fixed} alt={name} />
-			</div>
+			hello from projects
 		</Layout>
 	);
 };
 
-export const query = graphql`
-	query GetSingleProject($slug: String) {
-		project: contentfulProjects(slug: { eq: $slug }) {
-			name
-			descriptionImages {
-				fixed(width: 300) {
-					...GatsbyContentfulFixed
-				}
-			}
-			githubUrl
-			liveUrl
-			description {
-				description
-			}
-		}
-	}
-`;
+
 
 export default ProjectTemplate;
